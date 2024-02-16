7d8b91a7ffbc311afeb69735bc3e5afcbe3d8593
danujav <danujagreru@gmail.com>
2024-02-16 09:56:18 +0530

    Initiate a fxml form successfully

--- src/main/java/lk/ijse/javafx/AppInitializer.java
+++ src/main/java/lk/ijse/javafx/AppInitializer.java
@@ -1,7 +1,29 @@
 package lk.ijse.javafx;
 
-public class AppInitializer {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/main_form.fxml"));
+        Scene scene = new Scene(rootNode);
+
+        stage.setScene(scene);
+        stage.setTitle("Main Form");
+        stage.centerOnScreen();
+
+        stage.show();
 
     }
 }
---
+++ src/main/java/lk/ijse/javafx/AppInitializerWrapper.java
@@ -0,0 +1,7 @@
+package lk.ijse.javafx;
+
+public class AppInitializerWrapper {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
